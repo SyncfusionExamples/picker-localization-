b7b525346befadfe2d859214fbab84644df6ff96
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-27 20:12:28 +0530

    localization sample has been added

---
+++ PickerLocalization/PickerLocalization/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Syncfusion.SfPicker.XForms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PickerLocalization
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CultureInfo.CurrentUICulture = new CultureInfo("fr-FR");
+            PickerResourceManager.Manager = new ResourceManager("PickerLocalization.Resources.Syncfusion.SfPicker.XForms", Application.Current.GetType().Assembly);
+         
+            picker.ItemsSource = new string[]
+            {
+                "Red",
+                "Yellow",
+                "Black",
+                "Brown",
+                "White",
+                "Skyblue",
+                "Orange",
+                "Blue",
+                "Green",
+                "Pink"
+            };
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            picker.IsOpen = true;
+        }
+
+    }
+}
